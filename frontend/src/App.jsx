0503ad13c8f080919c76f1f75130eeb9d8a255ed
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-07 20:59:27 -0800

    Start on header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import { useState } from 'react'
 import { Footer, Header } from './components'
+import { pic1, pic2 } from './assets'
 
 function App() {
+  const slides = [pic1, pic2]
   return (
     <div className='App'>
-      <Header />
+      <div>
+        <Header>
+          {slides.map((s) => (
+            <div
+              className='flex-none h-screen w-screen'
+              style={{
+                backgroundImage: `url(${s})`,
+                width: '100%',
+                height: '84vh',
+                backgroundSize: 'cover',
+                backgroundPosition: '0% 25%',
+              }}
+            ></div>
+          ))}
+          {/* {slides.map((s) => (
+            <img src={s} />
+          ))} */}
+        </Header>
+      </div>
+
       <main className='min-h-[82vh]'>
         <div className='container flex justify-center'>
           <h1>Malcolm Smalls Beats</h1>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Header() {
+export default function Header({ children: slides }) {
+  const [curr, setCurr] = useState(0)
+  const goToIndex = (e) => {
+    setCurr(e.target.id)
+  }
+  const next = () => {
+    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+  }
   return (
     <header>
-      <div>header</div>
+      <main className='relative overflow-hidden'>
+        <div className='flex '>{slides}</div>
+        <div className='absolute bottom-4 right-0 left-0 flex items-center justify-center gap-2'>
+          {slides.map((item, i) => (
+            <div
+              key={i}
+              id={i}
+              onClick={(e) => goToIndex(e)}
+              className={`
+            transition-all w-2 h-2 bg-white rounded-full
+            ${curr === i ? 'p-2' : 'bg-opacity-50'}
+            `}
+            />
+          ))}
+        </div>
+      </main>
     </header>
   )
 }
