749cab591c798db9b2183ff7a360b706fe592686
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-18 11:31:27 -0700

    Connect to mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import beats from './data/beats.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
