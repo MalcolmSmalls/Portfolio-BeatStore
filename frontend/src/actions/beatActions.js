893d9e6348356f37a7bf56f5ff9dab482977afc5
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-21 20:59:25 -0700

    Add actions and constants

---
+++ frontend/src/actions/beatActions.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import {
+  BEAT_LIST_FAIL,
+  BEAT_LIST_REQUEST,
+  BEAT_LIST_SUCCESS,
+} from '../constants/beatConstants'
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: BEAT_LIST_REQUEST })
+
+    const { data } = await axios.get('/api/beats')
+
+    dispatch({ type: BEAT_LIST_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: BEAT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
--- frontend/src/reducers/beatReducers.js
+++ frontend/src/reducers/beatReducers.js
@@ -1,10 +1,16 @@
+import {
+  BEAT_LIST_FAIL,
+  BEAT_LIST_REQUEST,
+  BEAT_LIST_SUCCESS,
+} from '../constants/beatConstants'
+
 export const beatListReducer = (state = { beats: [] }, action) => {
   switch (action.type) {
-    case 'BEAT_LIST_REQUEST':
+    case BEAT_LIST_REQUEST:
       return { loading: true, beats: [] }
-    case 'BEAT_LIST_SUCCESS':
+    case BEAT_LIST_SUCCESS:
       return { loading: false, beats: action.payload }
-    case 'BEAT_LIST_FAIL':
+    case BEAT_LIST_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
