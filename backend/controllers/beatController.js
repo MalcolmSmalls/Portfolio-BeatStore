7d3331cd8147be5e41731d8b0b2e2a8613b8037e
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-23 01:13:03 -0700

    Clean up routes and add controllers to handle logic instead

---
+++ backend/controllers/beatController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler'
+import Beat from '../models/beatModel.js'
+
+// @desc Fetch all beats
+// @route GET /api/beats
+// @access all users
+const getBeats = asyncHandler(async (req, res) => {
+  const beats = await Beat.find({})
+
+  res.json(beats)
+})
+
+// @desc Fetch single beat
+// @route GET /api/beats/:id
+// @access all users
+const getBeatById = asyncHandler(async (req, res) => {
+  const beat = await Beat.findById(req.params.id)
+
+  if (beat) {
+    res.json(beat)
+  } else {
+    res.status(404)
+    throw new Error('Beat not found')
+  }
+})
+
+export { getBeats, getBeatById }
--- backend/routes/beatRoutes.js
+++ backend/routes/beatRoutes.js
@@ -1,34 +1,10 @@
 import express from 'express'
-import Beat from '../models/beatModel.js'
-import asyncHandler from 'express-async-handler'
+import { getBeats, getBeatById } from '../controllers/beatController.js'
+
 const router = express.Router()
 
-// @desc Fetch all beats
-// @route GET /api/beats
-// @access all users
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const beats = await Beat.find({})
-    res.json(beats)
-  })
-)
+router.route('/').get(getBeats)
 
-// @desc Fetch single beat
-// @route GET /api/beats/:id
-// @access all users
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const beat = await Beat.findById(req.params.id)
-
-    if (beat) {
-      res.json(beat)
-    } else {
-      res.status(404)
-      throw new Error('Beat not found')
-    }
-  })
-)
+router.route('/:id').get(getBeatById)
 
 export default router
