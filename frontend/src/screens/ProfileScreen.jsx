eb4696d0e36886192abdccd75a7f38a134e8772d
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-24 01:50:23 -0700

    Finish ProfileScreen Update Profile side

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -39,70 +39,80 @@ export default function ProfileScreen() {
     }
   }
   return (
-    <>
-      <h1 className='text-9xl mt-10 text-golden'>Update Profile</h1>
-      {error && <h2 className='text-red-500'>{error}</h2>}
-      {message && <h2 className='text-red-500'>{message}</h2>}
-      {loading && <h2>Loading...</h2>}
-      <form
-        onSubmit={submitHandler}
-        className='flex items-center flex-col w-[30%] font-Poppins'
-      >
-        <label htmlFor='name' className='text-sm uppercase font-bold'>
-          Name
-        </label>
-        <input
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder='Enter your name'
-          id='name'
-          className='border-2 rounded p-1 w-[80%] mb-5'
-        />
-        <label htmlFor='email' className='text-sm uppercase font-bold'>
-          E-Mail Address
-        </label>
-        <input
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder='Enter your email'
-          id='email'
-          className='border-2 rounded p-1 w-[80%] mb-5'
-        />
-        <label htmlFor='password' className='text-sm uppercase font-bold'>
-          Password
-        </label>
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='Enter your password'
-          id='password'
-          className='border-2 rounded p-1 w-[80%]'
-        />
+    <div className='w-screen flex'>
+      <div className='left w-[50%] text-lg flex flex-col items-center'>
+        <h3 className='text-5xl p-10 text-golden'>My Profile</h3>
+        {error && <h2 className='text-red-500'>{error}</h2>}
+        {message && <h2 className='text-red-500'>{message}</h2>}
+        {loading && <h2>Loading...</h2>}
+        <div className='flex items-center w-[70%]'>
+          <form
+            onSubmit={submitHandler}
+            className='flex items-start flex-col w-[100%] font-Poppins'
+          >
+            <label
+              htmlFor='name'
+              className='text-sm uppercase font-bold flex justify-start'
+            >
+              Name
+            </label>
+            <input
+              type='text'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder='Enter your name'
+              id='name'
+              className='border-2 rounded p-1 w-full mb-5'
+            />
+            <label htmlFor='email' className='text-sm uppercase font-bold'>
+              E-Mail Address
+            </label>
+            <input
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter your email'
+              id='email'
+              className='border-2 rounded p-1 w-full mb-5'
+            />
+            <label htmlFor='password' className='text-sm uppercase font-bold'>
+              Password
+            </label>
+            <input
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter your password'
+              id='password'
+              className='border-2 rounded p-1 w-full'
+            />
 
-        <label
-          htmlFor='confirmPassword'
-          className='text-sm uppercase font-bold'
-        >
-          Confirm Password
-        </label>
-        <input
-          type='password'
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder='Confirm your password'
-          id='confirmPassword'
-          className='border-2 rounded p-1 w-[80%]'
-        />
-        <button
-          type='submit'
-          className='uppercase block  bg-lighter-dark  text-white p-3 rounded-lg text-sm font-bold tracking-widest hover:bg-main-dark w-[80%] mt-5 mb-10 '
-        >
-          Update Profile
-        </button>
-      </form>
-    </>
+            <label
+              htmlFor='confirmPassword'
+              className='text-sm uppercase font-bold mt-5'
+            >
+              Confirm Password
+            </label>
+            <input
+              type='password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder='Confirm your password'
+              id='confirmPassword'
+              className='border-2 rounded p-1 w-full'
+            />
+            <button
+              type='submit'
+              className='uppercase block  bg-lighter-dark  text-white p-3 rounded-lg text-sm font-bold tracking-widest hover:bg-main-dark w-full mt-5 mb-10 '
+            >
+              Update Profile
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className='right border-2 w-[50%] text-lg'>
+        <h3 className='text-5xl p-10 text-golden'>My Orders</h3>
+      </div>
+    </div>
   )
 }
