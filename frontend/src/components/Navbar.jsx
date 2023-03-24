772189a5e2be2e420966f692780a2a5451a6326c
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-23 18:15:09 -0700

    Add ability to logout, add logout actions

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 export default function Navbar() {
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <nav>
       <div className='h-10 bg-white'>
@@ -27,10 +37,29 @@ export default function Navbar() {
             </Link>
           </li>
           <li>
-            <Link to='/login' className='hover:text-main-dark'>
-              Log-In
-            </Link>
+            {userInfo ? (
+              <Link to='/profile' className='hover:text-main-dark'>
+                My Profile
+              </Link>
+            ) : (
+              <Link to='/login' className='hover:text-main-dark'>
+                Log-In
+              </Link>
+            )}
           </li>
+
+          {userInfo ? (
+            <li>
+              <span
+                to='/profile'
+                className='hover:text-main-dark cursor-pointer'
+                onClick={logoutHandler}
+              >
+                Log-Out
+              </span>
+            </li>
+          ) : null}
+
           <li>
             <Link to='/cart' className='hover:text-main-dark'>
               Cart
