870629b3585166366e332f5de8fb4d17be590eda
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-26 12:46:52 -0700

    Finish backend order details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
-// @desc Create nw order
+// @desc Create new order
 // @route POST /api/orders
 // @access Private
 const addOrderItems = asyncHandler(async (req, res) => {
@@ -24,4 +24,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems }
+// @desc Get order by id
+// @route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
