a4e5456adbf8fd0b79cb058c4eead7f70dcf08ad
MalcolmSmalls <smallsbeats@gmail.com>
2023-04-17 12:10:37 -0700

    Fix responsiveness of login screen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -26,7 +26,7 @@ export default function LoginScreen() {
   }
   return (
     <FormContainer>
-      <h1 className='text-9xl mt-10 text-golden'>Log-In</h1>
+      <h1 className='text-9xl mt-10 mb-5 text-golden'>Log-In</h1>
       {error && <h2 className='text-red-500'>{error}</h2>}
       {loading && <h2>Loading...</h2>}
       <form
@@ -42,7 +42,7 @@ export default function LoginScreen() {
           onChange={(e) => setEmail(e.target.value)}
           placeholder='Enter your email'
           id='email'
-          className='border-2 rounded p-1 w-[80%] mb-5'
+          className='border-2 rounded p-1 lg:w-[80%] w-[13rem] mb-5'
         />
         <label htmlFor='password' className='text-sm uppercase font-bold'>
           Password
@@ -53,7 +53,7 @@ export default function LoginScreen() {
           onChange={(e) => setPassword(e.target.value)}
           placeholder='Enter your password'
           id='password'
-          className='border-2 rounded p-1 w-[80%]'
+          className='border-2 rounded p-1 lg:w-[80%] w-[13rem]'
         />
         <button
           type='submit'
